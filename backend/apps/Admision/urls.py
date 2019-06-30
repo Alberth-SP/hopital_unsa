22441d462bf5247ece4e6c0252c42f5c59fbbf90
Julio <jvicenteg.96@gmail.com>
2019-07-01 00:51:58 +0200

    API REST 100%

--- backend/apps/Admision/urls.py
+++ backend/apps/Admision/urls.py
@@ -1,8 +1,12 @@
+from django.conf.urls import url
 from django.urls import path
-from apps.Admision.views import *
+from rest_framework.urlpatterns import format_suffix_patterns
+from apps.Admision.views import listaCitas, reservarCita
 
 
-urlpatterns=[
-    path('nuevo',crearcitas)
-  
+urlpatterns = [
+    url('listar', listaCitas.as_view(),name = "listacitas"),
+    url('reservar', reservarCita.as_view() ,name = "reservacita")
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
